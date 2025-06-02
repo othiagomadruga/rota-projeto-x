e81ef96cae4fc78943358808f80df8f09fd8e282
Thiago Godoy <89321983+othiagomadruga@users.noreply.github.com>
2025-06-02 18:14:25 -0300

    Update database.go

--- database.go
+++ database.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -9,9 +11,15 @@ import (
 var db *sql.DB
 
 func InitDB() {
+	dbURL := "root:Thiago123@tcp(127.0.0.1:3306)/crud_go" // Mantenha ou ajuste para a variável de ambiente no Heroku
 	var err error
-	db, err = sql.Open("mysql", "USER:PASSWORD@tcp(HOST:PORT)/DATABASE")
+	db, err = sql.Open("mysql", dbURL)
 	if err != nil {
 		panic(err)
 	}
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Conectado ao banco de dados MySQL com sucesso!")
 }
